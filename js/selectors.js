add8ed1958ac4c68cd6a2472290843a553d72b2b
richard <richard.lee10@myhunter.cuny.edu>
2018-10-14 22:48:48 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,6 @@
-// declare your functions here...
-Function paragraphSelector(){
+// declare your functions here..
+
+function paragraphSelector(){
   var all_p_tags=$('p');
   return all_p_tags;
 }
